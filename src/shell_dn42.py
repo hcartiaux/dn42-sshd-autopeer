f6f2966098d0a90a4f31dd76517f536912b9e9f1
Hyacinthe Cartiaux <hyacinthe.cartiaux@free.fr>
2025-03-25 01:16:58 +0100

    ShellDn42 - cosmetic changes and comments

--- src/shell_dn42.py
+++ src/shell_dn42.py
@@ -6,7 +6,6 @@ from rich.table import Table
 from rich.text import Text
 from src.utils_dn42 import *
 
-
 class ShellDn42(Cmd):
     """
     A custom shell for managing DN42 network peering sessions.
@@ -63,41 +62,53 @@ class ShellDn42(Cmd):
         self._allowed_chars = set("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789=:?[]_-.+/ ")
         self._allowed_chars.update({'\x1b', '\x7f', '\r', '\n'})
 
-        # call the base constructor of cmd.Cmd, with our own stdin and stdout
+        # Call the base constructor of cmd.Cmd with our own stdin and stdout
         super(ShellDn42, self).__init__(stdin=stdin, stdout=stdout)
 
     def default(self, line):
+        """
+        Handle unknown commands.
+
+        :param line: The input line that was not recognized as a command.
+        """
         if line != 'EOF':
             self.sanitized_print('*** Unknown syntax: ' + line)
 
     def prompt_line(self):
-        """Reads an input until Enter is pressed"""
+        """
+        Reads an input until Enter is pressed.
+
+        :return: The input line read from the user.
+        """
         line, max_line_length = '', 80
         while len(line) < max_line_length:
             ch = self.stdin.read(1).decode("utf-8", "ignore")
             if ch not in self._allowed_chars:
                 continue
             if ch in {'\r', '\n'}:  # Enter key
-                self.stdout.write('\r\n') # Move to the next line
+                self.stdout.write('\r\n')  # Move to the next line
                 break
-            if ch == '\x7f' and len(line) > 0: # Backspace key
+            if ch == '\x7f' and len(line) > 0:  # Backspace key
                 line = line[:-1]
-                self.stdout.write('\b \b') # Erase the last character
-            elif ch == '\x7f' and len(line) == 0: # Backspace key and line is empty
+                self.stdout.write('\b \b')  # Erase the last character
+            elif ch == '\x7f' and len(line) == 0:  # Backspace key and line is empty
                 continue
             else:
-                self.stdout.write(ch) # Echo character
+                self.stdout.write(ch)  # Echo character
                 line += ch
         self.stdout.flush()
         return line
 
     def cmdloop(self, intro=True):
-        """Repeatedly issue a prompt, accept input, parse an initial prefix
+        """
+        Main command loop.
+
+        Repeatedly issue a prompt, accept input, parse an initial prefix
         off the received input, and dispatch to action methods, passing them
         the remainder of the line as argument.
 
+        :param intro: If True, display the introductory message.
         """
-
         self.preloop()
         if intro:
             self.do_intro()
@@ -119,6 +130,7 @@ class ShellDn42(Cmd):
         self.postloop()
 
     def print_topics(self, header, cmds, cmdlen, maxcol):
+        """Print command topics."""
         if cmds:
             self.stdout.write("%s\r\n" % str(header))
             if self.ruler:
@@ -126,28 +138,41 @@ class ShellDn42(Cmd):
             self.columnize(cmds, maxcol - 1)
             self.stdout.write("\r\n")
 
-    # If an empty line is given as input, we just print out a newline.
-    # This fixes a display issue when spamming enter.
     def emptyline(self):
+        """Handle empty line input."""
         self.sanitized_print('')
 
     #############
     # Sanitized print methods
     #############
-
     # These are custom print() functions that will let us utilize the given stdout.
+
     def print(self, value):
-        # make sure the stdout is set.
-        # we could add an else which uses the default print(), but I will not
+        """
+        Custom print function that utilizes the given stdout.
+
+        :param value: The value to print.
+        """
         if self.stdout and not self.stdout.closed:
             self.stdout.write(value)
             self.stdout.flush()
 
     def sanitized_print(self, output):
+        """
+        Print sanitized output.
+
+        :param output: The output string to sanitize and print.
+        """
         clean_output = '\r\n'.join(line.strip() for line in output.splitlines())
         self.print(clean_output + '\r\n')
 
     def rich_print(self, rich_object, newline=True):
+        """
+        Print rich objects.
+
+        :param rich_object: The rich object to print.
+        :param newline: Whether to print a newline at the end.
+        """
         console = Console(force_terminal=True)
         output = StringIO()
         console.file = output
@@ -158,6 +183,12 @@ class ShellDn42(Cmd):
             self.print(output.getvalue())
 
     def rich_prompt(self, text):
+        """
+        Display a rich prompt and read input.
+
+        :param text: The prompt text to display.
+        :return: The input line read from the user.
+        """
         self.rich_print(text, newline=False)
         return self.prompt_line()
 
@@ -167,13 +198,13 @@ class ShellDn42(Cmd):
 
     def do_intro(self, arg=None):
         """Display welcome message and user's AS information"""
-        self.emptyline()
         as_nums = as_maintained_by(self.username)
 
         # Welcome and connection details
-        welcome_text = f"Welcome to Flip Flap Network (AS{self.asn}) automated peering service\n"
-        welcome_text += f"You are connected as [bold blue]{self.username.upper()}-MNT[/] to [bold yellow]{self.server} @ AS{self.asn}"
-        self.rich_print(welcome_text)
+        self.emptyline()
+        self.rich_print(f"Welcome to Flip Flap Network (AS{self.asn}) automated peering service")
+        self.rich_print(f"You are connected as [bold blue]{self.username.upper()}-MNT[/] to [bold yellow]{self.server} @ AS{self.asn}")
+        self.emptyline()
 
         # Display AS numbers
         table = Table(style="blue")
@@ -188,7 +219,6 @@ class ShellDn42(Cmd):
         self.emptyline()
         self.rich_print('Type help or ? to list commands.')
 
-    # even if you don't use the arg parameter, it must be included.
     def do_bye(self, arg):
         "Quit the current shell"
         self.sanitized_print('See You, Space Cowboy!')
@@ -198,7 +228,6 @@ class ShellDn42(Cmd):
         return True
 
     def do_peer_create(self, arg):
-
         """Interactive process to create a new peering session"""
         peer_list = get_peer_list(self.username).keys()
         as_nums = as_maintained_by(self.username)
@@ -215,29 +244,28 @@ class ShellDn42(Cmd):
             return
 
         wg_pub_key       = self.rich_prompt("[bold blue]Wireguard public key      :[/] ")
-        if not match('^[0-9a-zA-Z+/]{43}=$', str(wg_pub_key)):
+        if not match('^[0-9a-zA-Z+/]{43}=$', wg_pub_key):
             self.rich_print('[red] :exclamation: Malformed WireGuard public key (^[0-9a-zA-Z+/]{43}=$)')
             return
 
         wg_endpoint_addr = self.rich_prompt("[bold blue]Wireguard endpoint address:[/] ")
-        if len(get_ipv6(wg_endpoint_addr)) == 0:
+        if not get_ipv6(wg_endpoint_addr):
             self.rich_print('[red] :exclamation: The endpoint address should be either an IPv6 address or a domain name with an AAAA record')
             return
 
         wg_endpoint_port = self.rich_prompt("[bold blue]Wireguard endpoint port   :[/] ")
-        if not match('^[0-9]+$', str(wg_endpoint_port)):
-            self.rich_print('[red] :exclamation:  Malformed port number (^[0-9]+$)')
+        if not match('^[0-9]+$', wg_endpoint_port):
+            self.rich_print('[red] :exclamation: Malformed port number (^[0-9]+$)')
             return
 
         if peer_create(as_num, wg_pub_key, wg_endpoint_addr, wg_endpoint_port):
-            self.rich_print('[green]The peering session has been created for AS' + as_num)
+            self.rich_print(f'[green]The peering session has been created for AS{as_num}')
             self.rich_print('[green] :information: Display the configuration information with the command [italic]peer_config[/italic]')
         else:
-            self.rich_print('[red] :exclamation:  The peering session could not be created for AS' + as_num)
+            self.rich_print(f'[red] :exclamation: The peering session could not be created for AS{as_num}')
 
     def do_peer_config(self, arg):
-        "Show your peering session configuration"
-
+        """Show your peering session configuration."""
         peer_list = get_peer_list(self.username).keys()
         if len(peer_list) == 1:
             as_num = next(iter(peer_list))
@@ -251,7 +279,7 @@ class ShellDn42(Cmd):
         peer_config = get_peer_config(self.username, as_num)
         table_remote = Table(style='blue')
         table_remote.add_column("Link config.", no_wrap=True)
-        table_remote.add_column("AS" + as_num, no_wrap=True)
+        table_remote.add_column(f"AS{as_num}", no_wrap=True)
         table_remote.add_row('Wg pub key', peer_config['wg_pub_key'])
         table_remote.add_row('Wg Endpoint addr.', Text(peer_config['wg_endpoint_addr']))
         table_remote.add_row('Wg Endpoint port', peer_config['wg_endpoint_port'])
@@ -261,7 +289,7 @@ class ShellDn42(Cmd):
         local_config = get_local_config(as_num)
         table_local = Table(style='yellow')
         table_local.add_column("Link config.", no_wrap=True)
-        table_local.add_column("AS" + self.asn, no_wrap=True)
+        table_local.add_column(f"AS{self.asn}", no_wrap=True)
         table_local.add_row('Wg pub key', local_config['wg_pub_key'])
         table_local.add_row('Wg Endpoint addr.', Text(local_config['wg_endpoint_addr']))
         table_local.add_row('Wg Endpoint port', local_config['wg_endpoint_port'])
@@ -269,17 +297,17 @@ class ShellDn42(Cmd):
         self.rich_print(table_local)
 
         table_wg = Table(style='blue')
-        table_wg.add_column("Wireguard configuration for AS" + as_num, no_wrap=True)
+        table_wg.add_column(f"Wireguard configuration for AS{as_num}", no_wrap=True)
         table_wg.add_row(Text(gen_wireguard_config(self.username, as_num)))
         self.rich_print(table_wg)
 
         table_bird = Table(style='blue')
-        table_bird.add_column("Bird configuration for AS" + as_num, no_wrap=True)
+        table_bird.add_column(f"Bird configuration for AS{as_num}", no_wrap=True)
         table_bird.add_row(Text(gen_bird_config(self.username, as_num)))
         self.rich_print(table_bird)
 
     def do_peer_list(self, arg):
-        "List your existing peering sessions"
+        """List your existing peering sessions."""
         self.emptyline()
         table = Table(title="Your existing peering sessions", style="blue")
         table.add_column("AS number", no_wrap=True)
@@ -295,7 +323,11 @@ class ShellDn42(Cmd):
         self.rich_print(table)
 
     def do_peer_remove(self, arg):
-        "Remove an already configured peering session"
+        """
+        Remove an already configured peering session.
+
+        :param arg: Optional argument (not used).
+        """
         peer_list = get_peer_list(self.username).keys()
         if len(peer_list) == 1:
             as_num = next(iter(peer_list))
@@ -306,18 +338,20 @@ class ShellDn42(Cmd):
                 self.rich_print('[green] :information: List your peering sessions with [italic]peer_list[/italic], create a new one with [italic]peer_create[/italic]')
                 return
 
-        confirm = self.rich_prompt("[bold red]Do you really want to remove the peering session of AS " + as_num + "? (YES/NO): ")
+        confirm = self.rich_prompt(f"[bold red]Do you really want to remove the peering session of AS{as_num}? (YES/NO): ")
         if confirm != 'YES':
             self.rich_print('[red] :exclamation: Abort peering session removal')
         elif peer_remove(as_num):
-            self.rich_print('Peering session of AS' + as_num + ' succesfully removed')
+            self.rich_print(f'Peering session of AS{as_num} successfully removed')
         else:
-            self.rich_print('[red] :exclamation: The peering session could not be removed for AS' + as_num)
-
+            self.rich_print(f'[red] :exclamation: The peering session could not be removed for AS{as_num}')
 
     def do_peer_status(self, arg):
-        "Print the state of a peering sessions"
+        """
+        Print the state of a peering session.
 
+        :param arg: Optional argument (not used).
+        """
         peer_list = get_peer_list(self.username).keys()
         if len(peer_list) == 1:
             as_num = next(iter(peer_list))
@@ -331,7 +365,7 @@ class ShellDn42(Cmd):
         cmd_output = peer_status(as_num)
         table = Table(style="yellow")
         table.add_column(
-            "Peering session status on " + self.server,
+            f"Peering session status on {self.server}",
             no_wrap=False)
         table.add_row(Text(cmd_output))
         self.rich_print(table)
