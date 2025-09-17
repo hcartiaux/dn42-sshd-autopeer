6ecf34f9c26d2d4f6a5647adf4a268bf052ff03c
Hyacinthe Cartiaux <hyacinthe.cartiaux@free.fr>
2025-09-17 18:07:33 +0200

    ShellDn42 - cosmetic changes

--- src/shell_dn42.py
+++ src/shell_dn42.py
@@ -322,12 +322,12 @@ class ShellDn42(Cmd):
             self.rich_print('[green] :information: List your peering sessions with [italic]peer_list[/italic], remove with [italic]peer_remove[/italic]')
             return
 
-        wg_pub_key       = self.rich_prompt("[bold blue]Wireguard public key      :[/] ")
+        wg_pub_key       = self.rich_prompt("[bold blue]WireGuard public key      :[/] ")
         if not match('^[0-9a-zA-Z+/]{43}=$', wg_pub_key):
             self.rich_print('[red] :exclamation: Malformed WireGuard public key (^[0-9a-zA-Z+/]{43}=$)')
             return
 
-        wg_endpoint_addr = self.rich_prompt("[bold blue]Wireguard endpoint address:[/] ")
+        wg_endpoint_addr = self.rich_prompt("[bold blue]WireGuard endpoint address:[/] ")
         ipv6_list = get_ipv6(wg_endpoint_addr)
         if not ipv6_list:
             self.rich_print('[red] :exclamation: The endpoint address should be either an IPv6 address or a domain name with an AAAA record')
@@ -339,7 +339,7 @@ class ShellDn42(Cmd):
                 self.rich_print(f'[red] :exclamation: The endpoint address {ipv6} is forbidden')
                 return
 
-        wg_endpoint_port = self.rich_prompt("[bold blue]Wireguard endpoint port   :[/] ")
+        wg_endpoint_port = self.rich_prompt("[bold blue]WireGuard endpoint port   :[/] ")
         if not match('^[0-9]+$', wg_endpoint_port):
             self.rich_print('[red] :exclamation: Malformed port number (^[0-9]+$)')
             return
@@ -398,9 +398,9 @@ class ShellDn42(Cmd):
         table_remote = Table(style='blue')
         table_remote.add_column("Link config.", no_wrap=True)
         table_remote.add_column(f"AS{as_num}", no_wrap=True)
-        table_remote.add_row('Wg pub key', peer_config['wg_pub_key'])
-        table_remote.add_row('Wg Endpoint addr.', Text(peer_config['wg_endpoint_addr']))
-        table_remote.add_row('Wg Endpoint port', peer_config['wg_endpoint_port'])
+        table_remote.add_row('WG pub key', peer_config['wg_pub_key'])
+        table_remote.add_row('WG Endpoint addr.', Text(peer_config['wg_endpoint_addr']))
+        table_remote.add_row('WG Endpoint port', peer_config['wg_endpoint_port'])
         table_remote.add_row('Link-local address', Text(peer_config['ll_address']))
         self.rich_print(table_remote)
 
@@ -410,9 +410,9 @@ class ShellDn42(Cmd):
         table_local = Table(style='yellow')
         table_local.add_column("Link config.", no_wrap=True)
         table_local.add_column(f"AS{self.asn}", no_wrap=True)
-        table_local.add_row('Wg pub key', local_config['wg_pub_key'])
-        table_local.add_row('Wg Endpoint addr.', Text(local_config['wg_endpoint_addr']))
-        table_local.add_row('Wg Endpoint port', local_config['wg_endpoint_port'])
+        table_local.add_row('WG pub key', local_config['wg_pub_key'])
+        table_local.add_row('WG Endpoint addr.', Text(local_config['wg_endpoint_addr']))
+        table_local.add_row('WG Endpoint port', local_config['wg_endpoint_port'])
         table_local.add_row('Link-local address', Text(local_config['ll_address']))
         self.rich_print(table_local)
 
@@ -440,16 +440,17 @@ class ShellDn42(Cmd):
         """
         self.emptyline()
         table = Table(title="Your existing peering sessions", style="blue")
-        table.add_column("AS number", no_wrap=True)
-        table.add_column("Wireguard public key")
-        table.add_column("Endpoint address", no_wrap=True)
-        table.add_column("Endpoint port", no_wrap=True)
+        table.add_column("AS Number", no_wrap=True)
+        table.add_column("WireGuard Public Key")
+        table.add_column("Endpoint Address", no_wrap=True)
+        table.add_column("Port", no_wrap=True)
         for as_num, peer_info in self.db_manager.get_peer_list(self.username).items():
             table.add_row(
                 as_num,
                 peer_info['wg_pub_key'],
                 Text(peer_info['wg_endpoint_addr']),
-                peer_info['wg_endpoint_port'])
+                peer_info['wg_endpoint_port']
+            )
         self.rich_print(table)
 
     def do_peer_remove(self, arg):
